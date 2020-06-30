74c78b5d25966ec6ad0c527dcc6cae3895cfe4f1
Thomas Gorszczyk <thomasgo77@gmail.com>
2020-06-30 17:15:13 +0200

    return last 100 ps5 hashtags

--- twit.js
+++ twit.js
@@ -1,12 +1,13 @@
 const Twit = require("twit");
 
-const T =  new Twit({
+const fs = require("fs");
+const T = new Twit({
   consumer_key: "ybFUSrzAXGkKdIyHV1NeXYLZb",
   consumer_secret: "xJabGs8lQYPdTphq3G7ZOXUlNVIj3ow8CQuji8JLKTnBJTZ1nQ",
   access_token: "1061773966510096384-7k4izjTZuxeD3ZNn9QYxCKm6O064H2",
   access_token_secret: "Vv7jUoiDjy7ZwpdD0dhkA9SHQrE9MAQ11uT7KuZ8QhELK"
 });
 
-T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-  console.log(data);
+T.get('search/tweets', { q: '#ps5 since:2020-06-11', count: 100 }).then((result) => {
+  fs.writeFileSync("./data/tweets.json", JSON.stringify(result.data, null, '\t'));
 });
