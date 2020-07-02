3ab02df731851b9d3002d711b0a35dffe47ecc93
Thomas Gorszczyk <thomasgo77@gmail.com>
2020-07-02 11:05:00 +0200

    added config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "ybFUSrzAXGkKdIyHV1NeXYLZb",
+  consumer_secret: "xJabGs8lQYPdTphq3G7ZOXUlNVIj3ow8CQuji8JLKTnBJTZ1nQ",
+  access_token: "1061773966510096384-7k4izjTZuxeD3ZNn9QYxCKm6O064H2",
+  access_token_secret: "Vv7jUoiDjy7ZwpdD0dhkA9SHQrE9MAQ11uT7KuZ8QhELK",
+}
--- twit.js
+++ twit.js
@@ -1,20 +1,15 @@
 const Twit = require("twit");
-
 const fs = require("fs");
-const T = new Twit({
-  consumer_key: "ybFUSrzAXGkKdIyHV1NeXYLZb",
-  consumer_secret: "xJabGs8lQYPdTphq3G7ZOXUlNVIj3ow8CQuji8JLKTnBJTZ1nQ",
-  access_token: "1061773966510096384-7k4izjTZuxeD3ZNn9QYxCKm6O064H2",
-  access_token_secret: "Vv7jUoiDjy7ZwpdD0dhkA9SHQrE9MAQ11uT7KuZ8QhELK",
-});
+const config = require("./config");
+const T = new Twit(config);
 
-let sinceId = null;
 const getData = async (location, sinceId) => {
   return T.get("search/tweets", {
     q: "#ps5 since:2020-06-11",
     count: 100,
     geocode: location,
     since_id: sinceId,
+    include_rts: false
   }).then((result) => {
     let array = [];
     result.data[Object.keys(result.data)[0]].forEach((value) => {
@@ -22,7 +17,6 @@ const getData = async (location, sinceId) => {
       return array;
     });
     sinceId = array.slice(-1).pop();
-    console.log(sinceId);
     return result.data;
   });
 };
@@ -38,8 +32,8 @@ const restructuredObject = (data, country) => {
 };
 
 const writeFile = async () => {
-  const USADataset = await getData("39.293281,-100.658108,1900km", 1278177055918694400);
-  const FRDataSet = await getData("46.693822,2.242383,553.49km", 1278192703574904800);
+  const USADataset = await getData("39.293281,-100.658108,1900km");
+  const FRDataSet = await getData("46.693822,2.242383,553.49km");
   fs.writeFileSync(
     "./data/tweets.json",
     JSON.stringify(
